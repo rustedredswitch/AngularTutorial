543667653971a5313fd5106769b0ec3896626745
Alessandro Rosa <alessandro.rosa@intre.it>
2023-07-04 18:05:06 +0200

    components lifecycle

--- src/app/prova/prova.component.ts
+++ src/app/prova/prova.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  AfterViewChecked,
+  AfterContentChecked,
+  AfterContentInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-prova',
   templateUrl: './prova.component.html',
-  styleUrls: ['./prova.component.css']
+  styleUrls: ['./prova.component.css'],
 })
-export class ProvaComponent {
+export class ProvaComponent
+  implements
+    OnInit,
+    AfterViewInit,
+    AfterViewChecked,
+    AfterContentChecked,
+    AfterContentInit
+{
+  constructor() {
+    console.log('costruttore');
+  }
 
+  ngOnInit(): void {
+    console.log('ngOnInit');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
 }
