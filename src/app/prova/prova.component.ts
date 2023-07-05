41584c8d87803d733573d9d4aa6d48a23b2fc25a
Alessandro Rosa <alessandro.rosa@intre.it>
2023-07-05 09:52:53 +0200

    string interpolation

--- src/app/prova/prova.component.ts
+++ src/app/prova/prova.component.ts
@@ -20,6 +20,16 @@ export class ProvaComponent
     AfterContentChecked,
     AfterContentInit
 {
+  cani = [
+    {
+      nome: 'Roger',
+      razza: 'Golden Retriever',
+      descrizione: `I Golden Retriever sono cani di taglia media con un mantello folto e dorato.
+      Sono noti per la loro dolcezza e amichevolezza, sono l'ideale per famiglie e bambini.
+      Sono intelligenti, socievoli e amano stare all'aria aperta.`,
+    },
+  ];
+
   constructor() {
     console.log('costruttore');
   }
