c0c40cd7ad0f980ac5327c678eab03370db842f3
Alessandro Rosa <alessandro.rosa@intre.it>
2023-07-07 10:38:51 +0200

    ngSwitch directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'AngularTutorial';
   isVisible: Boolean = true;
-
-  persone = [
-    { nome: 'Luca', cognome: 'Rossi', isOnline: true },
-    { nome: 'Marco', cognome: 'Verdi', isOnline: false },
-    { nome: 'Anna', cognome: 'Neri', isOnline: true },
-  ];
-
-  onClick(event: Event) {
-    this.title = 'Hai cliccato il bottone';
-    this.isVisible = !this.isVisible;
-  }
+  numero = "tre";
 }
