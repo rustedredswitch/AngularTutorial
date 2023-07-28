3f0bf0f1df232ed3f759d8de35a3df2d55890d0c
Alessandro Rosa <alessandro.rosa@intre.it>
2023-07-28 12:41:59 +0200

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProvaComponent } from './prova/prova.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
-import { FormsModule } from '@angular/forms';
+import { AboutComponent } from './componenti/about/about.component';
+import { ContactComponent } from './componenti/contact/contact.component';
 import { HighlightDirective } from './direttive/highlight/highlight.directive';
+import { ProvaComponent } from './prova/prova.component';
+import { HomeComponent } from './componenti/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, ProvaComponent, HighlightDirective],
+  declarations: [
+    AppComponent,
+    ProvaComponent,
+    HighlightDirective,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
