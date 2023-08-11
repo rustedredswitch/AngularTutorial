e03a989b5d0049a7cebee6cf967c394c147f5990
Alessandro Rosa <alessandro.rosa@intre.it>
2023-08-11 17:25:01 +0200

    observable

--- src/app/componenti/home/home.component.ts
+++ src/app/componenti/home/home.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+  sub: any;
 
+  ngOnInit(): void {
+    // new Observable((observer) => {
+    //   let count = 0;
+    //   setInterval(() => {
+    //     observer.next(count);
+    //     count++;
+    //   }, 1000);
+    // }).subscribe((number) => {
+    //   console.log(number);
+    // });
+
+    this.sub = interval(1000).subscribe((number) => {
+      console.log(number);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
 }
